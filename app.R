949d82a25452187534f900e377104a62540e7f15
ihellman <ian.hellman@gmail.com>
2024-01-17 15:47:19 -0700

    deleting defunct, commented out code

--- app.R
+++ app.R
@@ -350,60 +350,6 @@ observeEvent(
     
     reachCond_leaflet_map(reachConditions(), input$reachCondMapSelect)
     
-    # # Pull variable to plot from input select.  Input just shows benchmark name
-    # # to make select cleaner but "Condition" is appended to select col with value.
-    # conditionVar <- paste0(input$reachCondMapSelect, "Condition")
-    # indicatorVar <- input$reachCondMapSelect
-    # 
-    # 
-    # # Label that shows up on hover
-    # labels <- paste0(
-    #   "<strong>", reachConditions()$PointID, "</strong><br>",
-    #   reachConditions()$StreamName, "<br>",
-    #   indicatorVar,": ", reachConditions()[[indicatorVar]], "<br>",
-    #   "Condition: ", reachConditions()[[conditionVar]]) %>%
-    #   lapply(htmltools::HTML)
-    # 
-    # # Create a new grouping variable.  Ensures legend item order is correct.
-    # ord <- factor(c("Major", "Moderate", "Minimal"), levels = c("Major", "Moderate", "Minimal"))
-    # 
-    # 
-    # # Palet
-    # pal <- colorFactor(c("#f03b20", "#feb24c", "#ffeda0"), 
-    #                    levels = ord,
-    #                    ordered = TRUE)
-    # 
-    # # Map
-    # reachConditions() %>%
-    #   st_transform(crs = 4326) %>%
-    #   leaflet(
-    #     options = leafletOptions(
-    #       attributionControl=FALSE)
-    #   ) %>%
-    #   addTiles() %>%
-    #   addCircleMarkers(
-    #     radius = 7,
-    #     color = "black",
-    #     fillColor = ~pal(reachConditions()[[conditionVar]]),
-    #     stroke = TRUE,
-    #     weight = 1,
-    #     fillOpacity = 1,
-    #     label = ~labels
-    #   ) %>%
-    #   addProviderTiles("Esri.WorldTopoMap",
-    #                    group = "Esri.WorldTopoMap") %>%
-    #   addProviderTiles("Esri.WorldImagery",
-    #                    group = "Esri.WorldImagery") %>%
-    #   addLayersControl(
-    #     baseGroups = c("Esri.WorldTopoMap",
-    #                    "Esri.WorldImagery"),
-    #     # position it on the topleft
-    #     position = "topleft") %>%
-    #   addLegend(pal = pal, 
-    #             values = ord,
-    #             opacity = 1, 
-    #             title = conditionVar)
-    
   })
 
   # Download CSV button 
