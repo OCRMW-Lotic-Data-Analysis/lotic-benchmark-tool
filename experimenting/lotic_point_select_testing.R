23a3c16e487e7e434f707e6140cdb1e0e2b91771
ihellman <ian.hellman@gmail.com>
2024-05-24 15:56:21 -0600

    more point select testing

--- experimenting/lotic_point_select_testing.R
+++ experimenting/lotic_point_select_testing.R
@@ -1,13 +1,17 @@
+library(dplyr)
+library(sf)
 source("C:/Users/ianhe/OneDrive - The University of Montana/EMMA/analysis/lotic-benchmark-tool/R/leaflet_maps.R")
 
 pts <- vroom::vroom("C:/Users/ianhe/OneDrive - The University of Montana/EMMA/analysis/lotic-benchmark-tool/appData/wy_landerPts.csv", delim = ",", show_col_types = FALSE) %>% 
   st_as_sf(coords = c("SampledMidLongitude", "SampledMidLatitude"), crs = 4269)
 
+pts$id <- seq_len(nrow(pts))
 
 indicator_leaflet_map(pts, "PointSelectionType")
 
 
-
+indicator_leaflet_map(pts, "PointSelectionType") %>%
+  addAwesomeMarkers(layerId = ~id, icon = icons)
 
 ui <- fluidPage(
   titlePanel("Leaflet Map"),
@@ -30,9 +34,9 @@ server <- function(input, output, session) {
   RV <- reactiveValues(points = points)
   
   icons <- awesomeIcons(
-    icon = 'ios-close',
+    icon = 'home',
     iconColor = 'white',
-    library = 'ion',
+    library = 'glyphicon',
     markerColor = "blue"
   )
   
@@ -43,34 +47,54 @@ server <- function(input, output, session) {
   #     addAwesomeMarkers(data = points, lng = ~X, lat = ~Y, layerId = ~id, icon = icons)
   # })
   
+  # output$mymap <- renderLeaflet({
+  #   indicator_leaflet_map(points, "PointSelectionType") %>%
+  #     addAwesomeMarkers(layerId = ~id, icon = icons)
+  # })
+  
   output$mymap <- renderLeaflet({
-    indicator_leaflet_map(points, "PointSelectionType") %>%
-      addAwesomeMarkers(layerId = ~id, icon = icons)
+    indicator_leaflet_map(points, "PointSelectionType") 
   })
   
   myLeafletProxy <- leafletProxy(mapId = "mymap", session)
   
   observeEvent(input$mymap_marker_click,{
     clicked_point <- input$mymap_marker_click
+    print(clicked_point)
     RV$points[points$id==clicked_point$id,]$clicked <- !(RV$points[points$id==clicked_point$id,]$clicked)
     
     updateTextInput(inputId = "selected_photos", value = paste(unlist(RV$points$id[which(RV$points$clicked)]), collapse = ", "))
     
     removeMarker(map = myLeafletProxy, layerId = clicked_point$id)
-    addAwesomeMarkers(map = myLeafletProxy,
-                      lng = clicked_point$lng,
-                      lat = clicked_point$lat,
-                      layerId = clicked_point$id,
-                      icon = awesomeIcons(
-                        icon = 'ios-close',
-                        iconColor = 'white',
-                        library = 'ion',
-                        markerColor = ifelse(RV$points[clicked_point$id,]$clicked, yes = "red", no = "blue")
-                      ))
+    # addAwesomeMarkers(map = myLeafletProxy,
+    #                   lng = clicked_point$lng,
+    #                   lat = clicked_point$lat,
+    #                   layerId = clicked_point$id,
+    #                   icon = awesomeIcons(
+    #                     icon = 'home',
+    #                     iconColor = 'white',
+    #                     library = 'glyphicon',
+    #                     markerColor = ifelse(RV$points[clicked_point$id,]$clicked, yes = "red", no = "blue")
+    #                   ))
+    
+    addCircleMarkers(
+      map = myLeafletProxy,
+      lng = clicked_point$lng,
+      lat = clicked_point$lat,
+      layerId = clicked_point$id,
+      radius = 15,
+      color = "black",
+      fillColor = ifelse(RV$points[clicked_point$id,]$clicked, yes = "red", no = "blue"),
+      stroke = TRUE, 
+      weight = 1,
+      fillOpacity = 1)
+    
+    
   })
   
   output$checker <- renderTable({
     #glimpse(RV$points)
+    #glimpse(clicked_point)
     })
 }
 
